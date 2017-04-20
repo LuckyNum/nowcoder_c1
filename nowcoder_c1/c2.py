c4761df3a6a0d0b8d96c7c7ae83ffe2842f094b8
lch <lch@163.com>
2017-04-20 12:03:10 +0800

    随机数+正则

--- nowcoder_c1/c2.py
+++ nowcoder_c1/c2.py
@@ -1,4 +1,6 @@
 # -*- encoding = UTF-8 -*-
+import random
+import re
 
 class User:
     type = 'USER'
@@ -30,10 +32,40 @@ def creat_user(type):
         return Guest('gu1', 1)
         # raise ValueError('error')
 
+def demo_exception():
+    try:
+        print 2/1
+        # print 2/0
+        raise Exception('Raise Error', 'NowCoder')
+    except Exception as e:
+        print 'error:', e
+    finally:
+        print 'clean up'
+
+def demo_random():
+    # 1 - 100
+    random.seed(1)
+
+    print 1, int(random.random() * 100)
+    print 2, random.randint(0, 200)
+    print 3, random.choice(range(0, 100, 10))
+    print 4, random.sample(range(0, 100), 4)
+    a = [1, 2, 3, 4, 5]
+    random.shuffle(a)
+    print 5, a
+
+def demo_re():
+    str = 'abc123def456gf45'
+    p1 = re.compile('[\d]+')
+    print 1, p1.findall(str)
+
 if __name__ == '__main__':
-    user1 = User('zhangsan', 1)
-    print user1
-    admin1 = Admin('lisi', 2, "groupA")
-    print admin1
-    print creat_user('USER')
-    print creat_user('XXX')
+    # user1 = User('zhangsan', 1)
+    # print user1
+    # admin1 = Admin('lisi', 2, "groupA")
+    # print admin1
+    # print creat_user('USER')
+    # print creat_user('XXX')
+    # demo_exception()
+    # demo_random()
+    demo_re()
