2948695d5e3e3ddae369bc65590d71803817c2e7
lch <lch@163.com>
2017-04-20 18:34:49 +0800

    模板+JinJa2+request

--- nowcoder_c1/c3.py
+++ nowcoder_c1/c3.py
@@ -1,7 +1,9 @@
 #-*- encoding:UTF-8 -*-
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+# 开头，行语法表达(line statements)
+app.jinja_env.line_statement_prefix = '#'
 
 @app.route('/index/')
 @app.route('/')
@@ -10,7 +12,17 @@ def index():
 
 @app.route('/profile/<int:uid>/', methods=['GET', 'POST'])
 def profile(uid):
-    return 'profile:' + str(uid)
+    # return 'profile:' + str(uid)
+    colors = ('red', 'green')
+    dicts = {'name': 'zhangsan', 'age': '12'}
+    return render_template('profile.html', uid = uid, colors = colors, dicts = dicts)
+
+@app.route('/request/')
+def demo_request_response():
+    res = ''
+    for property in dir(request):
+        res = res + str(property) + '#<br>' + str(eval('request.' + property)) + '<br>'
+    return res
 
 if __name__ == '__main__':
     app.run(debug=True)
